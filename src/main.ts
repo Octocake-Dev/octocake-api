c5d9f7e865019c7c45df76dc76043877c95fc417
imad <imadatyatalah@gmail.com>
2021-07-20 12:47:21 +0100

    feat: Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.enableCors({ origin: 'http://localhost:3000/' });
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
