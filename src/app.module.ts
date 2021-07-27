37aedf91594dd4cff0541e0ad08d1d610c892229
imad <imadatyatalah@gmail.com>
2021-07-27 16:18:48 +0100

    feat: Added app.config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { PostsModule } from './posts/posts.module';
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PostsModule],
+  imports: [ConfigModule.forRoot({ load: [appConfig] }), PostsModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
@@ -6,8 +7,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  const configService = app.get<ConfigService>(ConfigService);
+
   app.enableCors({ origin: 'http://localhost:3000/' });
 
-  await app.listen(process.env.PORT || 1337);
+  await app.listen(
+    configService.get<number>('app.port'),
+    configService.get<string>('app.host'),
+  );
 }
 bootstrap();
